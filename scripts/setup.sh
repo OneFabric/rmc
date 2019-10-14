cdfd978ec046afcf04ee897df68a17800dcc497f
OneFabric <1neFabric@gmail.com>
2019-10-14 15:05:41 -0700

    Changed runtime frequency.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# make script run once a day. Resource: https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
-(crontab -l 2>/dev/null; echo "00 00 * * * ~/Desktop/rmc/scripts/main.sh") | crontab -
+# make script run every 2 hours. Resource: https://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
+(crontab -l 2>/dev/null; echo "0 */2 * * * ~/Desktop/rmc/scripts/main.sh $1") | crontab -
