b586bb65aa5c5104aca5169e432b265050b8400c
OneFabric <1neFabric@gmail.com>
2019-10-14 11:18:42 -0700

    For loop to eventually check availability working.

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,17 +5,23 @@
 # TODO: make movie name configurable at run time
 
 # TODO: loop through movies.txt and obtain list of movies to query
+    
     # #https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
     while IFS= read -r line
     do movieList[index]=$line; ((index=index+1)) 
     done < ../input/movies.txt
 
     # testing 
-    echo ${movieList[@]} # prints contents of array
+    # echo ${movieList[@]} # prints contents of array
 
+# TODO: For each movie...
 
-# TODO: query API to obtain list of movies 
+    for movie in "${movieList[@]}"
+    do
+    # echo "$movie" # print each movie
+    done
+    # TODO: query API to obtain list of movies 
 
-# TODO: parse resulting data to obtain information
+    # TODO: parse resulting data to obtain information
 
 # TODO: conditionally send email to myself
