18c15221f75131c1520500291fc7b53dff6550a2
OneFabric <1neFabric@gmail.com>
2019-10-14 11:13:56 -0700

    TODO#3 works

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,6 +5,14 @@
 # TODO: make movie name configurable at run time
 
 # TODO: loop through movies.txt and obtain list of movies to query
+    # #https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
+    while IFS= read -r line
+    do movieList[index]=$line; ((index=index+1)) 
+    done < ../input/movies.txt
+
+    # testing 
+    echo ${movieList[@]} # prints contents of array
+
 
 # TODO: query API to obtain list of movies 
 
